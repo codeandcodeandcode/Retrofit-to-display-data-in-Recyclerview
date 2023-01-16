55df9c26d699b3c2f3bedb0f56dcdec65a166391
codeandcodeandcode <codepkcode49@gmail.com>
2023-01-16 11:28:29 +0300

    Added Comments on the APA interface

--- app/src/main/java/com/example/kotlinretrofitgetrqstbyyashnagayach/Apinterface.kt
+++ app/src/main/java/com/example/kotlinretrofitgetrqstbyyashnagayach/Apinterface.kt
@@ -7,6 +7,10 @@ import retrofit2.http.GET
 interface ApiInterface {
 
     @GET( "posts")
+    //@GET("Insert end point")
 
     fun getData(): Call<List<MyDataItem>>
+
+    //List is the data type.
+///<Insert type>
 }
